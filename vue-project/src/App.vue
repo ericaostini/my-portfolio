3c658a864fea81a35a9c64cf5e91f6333ffd468e
ericaostini <ericaostini14@gmail.com>
2024-03-27 11:08:13 +0100

    vue setup

---
+++ vue-project/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="text-danger">
+    ciao
+    <i class="fa-solid fa-user"></i>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped></style>
